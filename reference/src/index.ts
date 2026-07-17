b6b26d4ad15dd5a914ea2020d23f01c89c949af3
benamtech <ben@amtechai.com>
2026-07-17 07:13:41 -0400

    Export complement selection controls

--- reference/src/index.ts
+++ reference/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./core.js";
 export * from "./benchmark.js";
 export * from "./optimizer.js";
+export * from "./csi.js";
 export * from "./framework.js";
 export * from "./distribution.js";
 export * from "./generator.js";
