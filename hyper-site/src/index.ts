7cb0e9d2885f1f145c91773b3c6f21da67186b6e
benamtech <ben@amtechai.com>
2026-07-19 02:48:30 -0400

    H3 MVP: export living surface runtime

--- hyper-site/src/index.ts
+++ hyper-site/src/index.ts
@@ -2,3 +2,4 @@ export * from "./framework-core.js";
 export * from "./site-manifest.js";
 export * from "./browser-targets.js";
 export * from "./css-modern.js";
+export * from "./living-surface.js";
